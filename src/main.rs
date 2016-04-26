184d100d6eda14280da061dc36cb32d4a629e7b0
Alex Crichton <alex@alexcrichton.com>
2016-04-25 17:39:16 -0700

    Add lots of comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! A small wrapper script used by the Rust project on the buildbots to isolate
+//! builds.
+//!
+//! By default interrupting a build on buildbot doesn't actually kill the whole
+//! process tree, just the process at the top. This means that process left
+//! around (like compilers, linkers, etc) could continue to hold files open and
+//! cause future builds to fail as they may be hung and fail to exit.
+//!
+//! The purpose of this script is to ensure that the entire process tree is
+//! killed if this script is killed. This is done through the use of job objects
+//! on Windows.
+
 extern crate kernel32;
 extern crate psapi;
 extern crate winapi;
@@ -20,16 +42,32 @@ macro_rules! status {
 
 fn main() {
     unsafe {
+        // First up, create our job object.
         let job = create_job();
 
+        // Next, actually execute what we're supposed to do.
         let args = env::args_os().skip(1).collect::<Vec<_>>();
         let status = Command::new(&args[0]).args(&args[1..]).status().unwrap();
 
+        // This is a litte subtle. By default if we are terminated then all
+        // processes in our job object are terminated as well, but we
+        // intentionally want to whitelist some processes to outlive our job
+        // object (see below).
+        //
+        // To allow for this, we manually kill processes instead of letting the
+        // job object kill them for us. We do this in a loop to handle processes
+        // spawning other processes.
+        //
+        // Finally once this is all done we know that the only remaining ones
+        // are ourselves and the whitelisted processes. The `close_job` then
+        // configures our job object to *not* kill everything on close, then
+        // closes the job object.
         while kill_processes(&job) {
-            // ...
+            status!("killed some processes, going back to look for more");
         }
         close_job(job);
 
+        // Try to hav the same return code as the child we spawned.
         std::process::exit(status.code().unwrap_or(1))
     }
 }
@@ -48,6 +86,12 @@ impl Drop for Handle {
     }
 }
 
+/// Create a new job object, configure it to kill everything if the job object
+/// is closed, and then add ourselves to the job object.
+///
+/// Once we've added ourselves to the job object we're guaranteed that if we are
+/// abnormally terminated then our entire process tree is terminated. By default
+/// all our spawned children are part of our job object as well.
 unsafe fn create_job() -> Handle {
     use kernel32::*;
     use winapi::*;
@@ -73,6 +117,13 @@ unsafe fn create_job() -> Handle {
     Handle(job)
 }
 
+/// Kill all active processes in a job object, except those in our whitelist.
+///
+/// There are a few processes we intentionally leak outside the job object (see
+/// below for details), so this function will kill as much as possible in the
+/// job object excluding ourselves and those on the whitelist.
+///
+/// Returns whether any processes were killed or not.
 unsafe fn kill_processes(job: &Handle) -> bool {
     use kernel32::*;
     use psapi::*;
@@ -95,26 +146,29 @@ unsafe fn kill_processes(job: &Handle) -> bool {
     }
 
     let mut killed = false;
-
     let list = &jobs.list[..jobs.header.NumberOfProcessIdsInList as usize];
     assert!(list.len() > 0);
     status!("found {} remaining processes", list.len() - 1);
+
     for &id in list {
         // let's not kill ourselves
         if id as DWORD == GetCurrentProcessId() {
             continue
         }
 
+        // Open the process with the necessary rights, and if this fails then we
+        // probably raced with the process exiting so we ignore the problem.
         let p = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_TERMINATE |
                                 SYNCHRONIZE,
                             FALSE,
                             id as DWORD);
-        // If we couldn't open the process, then perhaps it exited?
         if p.is_null() {
             continue
         }
         let p = Handle(p);
 
+        // Load the file which this process was spawned from. We then later use
+        // this for identification purposes.
         let mut buf = [0; 1024];
         let r = GetProcessImageFileNameW(p.0, buf.as_mut_ptr(), buf.len() as DWORD);
         if r == 0 {
@@ -123,6 +177,9 @@ unsafe fn kill_processes(job: &Handle) -> bool {
         let s = OsString::from_wide(&buf[..r as usize]);
         status!("found remaining: {} - {:?}", id, s);
 
+        // Test if this process was actually in the job object or not. If it's
+        // not then we likely raced with something else recycling this PID, so
+        // we just skip this step.
         let mut res = 0;
         let r = IsProcessInJob(p.0, job.0, &mut res);
         if r == 0 {
@@ -133,6 +190,15 @@ unsafe fn kill_processes(job: &Handle) -> bool {
             continue
         }
 
+        // And here's where we find the whole purpose for this function!
+        // Currently, our only whitelisted process is `mspdbsrv.exe`, and more
+        // details about that can be found here:
+        //
+        //      https://github.com/rust-lang/rust/issues/33145
+        //
+        // The gist of it is that all builds on one machine use the same
+        // `mspdbsrv.exe` instance. If we were to kill this instance then we
+        // could erroneously cause other builds to fail.
         if let Some(s) = s.to_str() {
             if s.contains("mspdbsrv") {
                 status!("\toops, this is mspdbsrv");
@@ -140,6 +206,9 @@ unsafe fn kill_processes(job: &Handle) -> bool {
             }
         }
 
+        // Ok, this isn't mspdbsrv, let's kill the process. After we kill it we
+        // wait on it to ensure that the next time around in this function we're
+        // not going to see it again.
         status!("\tkilling still-running process");
         let r = TerminateProcess(p.0, 1);
         if r == 0 {
@@ -155,6 +224,16 @@ unsafe fn kill_processes(job: &Handle) -> bool {
     return killed
 }
 
+/// Prepare for closing the job object.
+///
+/// When we close this job object, it's by default configured to kill every
+/// process that's a member of it. Note that processes on our whitelist,
+/// however, are still in this job object, so closing it will cause them to go
+/// away.
+///
+/// We intentionally want to "leak" those processes, so reconfigure our job
+/// object to *not* kill anything when the job object is closed. After that we
+/// just drop it and close it and nothing should get killed abnormally.
 unsafe fn close_job(job: Handle) {
     use kernel32::*;
     use winapi::*;
